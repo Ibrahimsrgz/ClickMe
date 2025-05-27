deb1131aa6671af75e82cbc47ddc5632ad321ce7
ibrahim.sarigoz <ibrahim.sarigoz@IBRAHIM_S-PC>
2025-05-27 09:21:12 +0300

    forma yönlendirdik

--- click_server.py
+++ click_server.py
@@ -20,7 +20,7 @@ app = Flask(__name__)
 @app.route("/kullanici", defaults={"kullanici": None})
 @app.route("/kullanici=<kullanici>")
 def handle_request(kullanici):
-    return redirect("https://google.com", code=302)
+    return redirect("https://docs.google.com/forms/d/e/1FAIpQLSdWu-G5nK4TKD-bR_x3BMZ8l2vwjbyJ71kUxFoZdaeCVw6bGw/viewform?usp=dialog", code=302)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000)
