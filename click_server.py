6b14d10f9daf229cc9fa9b11e0c5cf10499dcae5
ibrahim.sarigoz <ibrahim.sarigoz@IBRAHIM_S-PC>
2025-05-27 08:17:42 +0300

    numara gelmeli endpoint de eklendi

--- click_server.py
+++ click_server.py
@@ -1,23 +1,30 @@
+#from flask import Flask, redirect, request
+#import os
+#import time
+#app = Flask(__name__)
+
+#@app.route("/")
+#def home():
+
+#    return redirect("https://google.com", code=302)
+
+#if __name__ == "__main__":
+#    app.run(host="0.0.0.0", port=8000)
+
 from flask import Flask, redirect, request
-import os
 import time
 
 app = Flask(__name__)
 
-LOG_PATH = "/tmp/clicks.txt"
-
-# Dosya varsa dokunma, yoksa oluştur
-if not os.path.exists(LOG_PATH):
-    with open(LOG_PATH, "w") as f:
-        f.write("Click Logs\n")
-
-@app.route("/")
-def home():
-    log_line = f"{request.remote_addr} - {time.ctime()}\n"
-    with open(LOG_PATH, "a") as f:
-        f.write(log_line)
-    print(log_line.strip())  # Render log ekranında görelim
+@app.route("/", defaults={"number": None})
+@app.route("/number", defaults={"number": None})
+@app.route("/number=<int:number>")
+def handle_request(number):
     return redirect("https://google.com", code=302)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000)
+
+
+
+
